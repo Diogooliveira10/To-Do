217fb71ccd1ba18cd144c3bb192acfa0d647dfbd
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2022-08-01 20:47:40 -0300

    Criando conexão com Banco

--- backend/src/todos.routes.js
+++ backend/src/todos.routes.js
@@ -2,12 +2,19 @@ const express = require("express");
 
 const allTodos = [{ name: "fulano", status: false }];
 const todosRoutes = express.Router();
+const { PrismaClient } = require("@prisma/client");
 
-todosRoutes.post("/todos", (req, res) => {
+const prisma = new PrismaClient();
+
+todosRoutes.post("/todos", async (req, res) => {
     const { name } = req.body;
-    allTodos.push({ name, status: false });
+    const todo = await prisma.todo.create({
+        data: {
+            name,
+        },
+    });
 
-    return res.status(201).json(allTodos);
+    return res.status(201).json(todo);
 });
 
 todosRoutes.get("/todos", (req, res) => {
